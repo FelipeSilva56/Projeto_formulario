8560d891882ebf3c4d864405872d1a934873379c
Felipe Pereira <felipep.silva5741@gmail.com>
2022-01-16 21:58:36 -0300

    Atualização JS
    Inseri mais capacidade de armazenamento de dados ao formulário

--- formulario/script.js
+++ formulario/script.js
@@ -5,12 +5,20 @@ const usuarios = []
 function recebeDados(e){
     e.preventDefault();
 
-    const nome = form.querySelector('#nome1')
-    const sobrenome = form.querySelector('#nome')
+    const nome = form.querySelector('#nome1');
+    const sobrenome = form.querySelector('#nome');
+    const email = form.querySelector('#email1');
+    const vaga = form.querySelector('#vaga');
+    const data = form.querySelector('#data_inicio');
+    const situ = form.querySelector('input[name="situ"]:checked');
 
     usuarios.push({
         nome:nome.value,
         sobrenome:sobrenome.value,
+        email:email.value,
+        vaga:vaga.value,
+        data:data.value,
+        situ:situ.value,
     })
     console.log(usuarios)
 }
